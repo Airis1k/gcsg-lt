2e7004096ef4785c8d8001375ca5be402f654eb1
Airis1k <airingasstar@gmail.com>
2024-09-03 13:38:53 +0300

    feat: added hero carousel & latest news

---
+++ src/pages/Home/latest-news/constants.ts
@@ -0,0 +1,65 @@
+type singleNew = {
+   id: number;
+   src: string;
+   title: string;
+   date: string;
+   author: string;
+   authorImg: string;
+};
+
+export const NEWS: singleNew[] = [
+   {
+      id: 1,
+      src: "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
+      title: "Boost your conversion rate",
+      date: "2020-03-16",
+      author: "Michael Foster",
+      authorImg:
+         "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+   },
+   {
+      id: 2,
+      src: "https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80",
+      title: "How to use search engine optimization to drive sales",
+      date: "2020-03-10",
+      author: "Lindsay Walton",
+      authorImg:
+         "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+   },
+   {
+      id: 3,
+      src: "https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80",
+      title: "Improve your customer experience",
+      date: "2020-02-04",
+      author: "Tom cook",
+      authorImg:
+         "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+   },
+   {
+      id: 4,
+      src: "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
+      title: "Boost your conversion rate",
+      date: "2020-03-16",
+      author: "Michael Foster",
+      authorImg:
+         "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+   },
+   {
+      id: 5,
+      src: "https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80",
+      title: "How to use search engine optimization to drive sales",
+      date: "2020-03-10",
+      author: "Lindsay Walton",
+      authorImg:
+         "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+   },
+   {
+      id: 6,
+      src: "https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80",
+      title: "Improve your customer experience",
+      date: "2020-02-04",
+      author: "Tom cook",
+      authorImg:
+         "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+   },
+];
